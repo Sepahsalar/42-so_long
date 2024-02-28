c1c5a2cdf81cb0d0afbb82f8d5ba5a6ffce6f1a0
Sepahsalar <alirezasohrabizadeh@gmail.com>
2024-02-28 16:02:46 +0200

    Add a test

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:42:23 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/02/28 10:29:37 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/02/28 16:01:26 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,27 @@
 
 #include <string.h>
 
-// Bytes Per Pixel. Since each pixel is represented as an integer,
-// it will be four bytes for four channels.
-#define BPP sizeof(int32_t)
-
-int32_t	main(void)
+int	main(void)
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
-	// Init mlx with a canvas size of 256x256
-	// and the ability to resize the window.
-	mlx = mlx_init(256, 256, "./so_long", true);
+
+	mlx = mlx_init(500, 500, "title", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
-	// Create a 128x128 image.
-	img = mlx_new_image(mlx, 128, 128);
-	// Set the channels of each pixel in our image
-	// to the maximum byte value of 255. 
-	ft_memset(img->pixels, 255, img->width * img->height * BPP);
-	// Draw the image at coordinate (0, 0).
-	mlx_image_to_window(mlx, img, 0, 0);
-	// Run the main loop and terminate on quit.  
+	img = mlx_new_image(mlx, 100, 100);
+	if (!img)
+	{
+		mlx_terminate(mlx);
+		exit(EXIT_FAILURE);
+	}
+	ft_memset(img->pixels, 255, img->width * img->height * sizeof(int));
+	if (mlx_image_to_window(mlx, img, 200, 200) < 0)
+	{
+		mlx_terminate(mlx);
+		exit(EXIT_FAILURE);
+	}
 	mlx_loop(mlx);
 	// mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
-
-
